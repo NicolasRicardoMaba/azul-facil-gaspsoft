2865266d40a3c6749531952548aa2666dce488ba
nicolas <nicolas.rm2005@aluno.ifsc.edu.br>
2022-11-21 14:50:54 -0300

    Estrutura base mvc

---
+++ src/model/IPessoaDAO.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public interface IPessoaDAO {
+
+	public boolean inserir(Pessoa p);
+
+	public boolean alterar(Pessoa p, long cpf);
+
+	public boolean excluir(Pessoa p, long cpf);
+
+	public ArrayList<Pessoa> listarPessoas();
+
+}
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -2,4 +2,32 @@ package model;
 
 public class Pessoa {
 
+	private String nome;
+	private Long cpf;
+
+	public Pessoa(String nome, Long cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public Pessoa() {
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
 }
